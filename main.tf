df6a4c1125f0b023892b697c6b425499bb728686
Wahiduzzaman Siddique <wahiduzzaman.siddique@nordcloud.com>
2022-03-01 15:07:16 +0200

    cluster created with namespaces

--- main.tf
+++ main.tf
@@ -8,20 +8,31 @@ terraform {
 }
 
 provider "google" {
-  project = "**********" // Use project name
+  project = "test-project-ws-342013" // Use project name
   region  = "us-central1"
 }
+
+provider "kubernetes" {
+
+  host  = "https://${local.cluster-endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    local.cluster-certificate,
+  )
+}
+
+data "google_client_config" "default" { }
 
 resource "google_compute_network" "vpc_network_gke" {
   name                    = "gke-vpc"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_network" "vpc_network_redis" {
-  name                    = "redis-vpc"
-  auto_create_subnetworks = false
-}
-
+# resource "google_compute_network" "vpc_network_redis" {
+  # name                    = "redis-vpc"
+  # auto_create_subnetworks = false
+# }
+# 
 resource "google_compute_subnetwork" "gke-subnet" {
   name          = "gke-vpc-subnet"
   ip_cidr_range = "10.2.0.0/16"
@@ -29,18 +40,18 @@ resource "google_compute_subnetwork" "gke-subnet" {
   network       = google_compute_network.vpc_network_gke.name
 }
 
-resource "google_compute_subnetwork" "redis-subnet" {
-  name          = "redis-vpc-subnet"
-  ip_cidr_range = "10.3.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.vpc_network_redis.name
-}
+# resource "google_compute_subnetwork" "redis-subnet" {
+  # name          = "redis-vpc-subnet"
+  # ip_cidr_range = "10.3.0.0/16"
+  # region        = "us-central1"
+  # network       = google_compute_network.vpc_network_redis.name
+# }
 
 
 resource "google_container_cluster" "primary" { // creates google kubernetes cluster
   name               = "gke-test-cluster"
   location           = "us-central1-a"
-  initial_node_count = 3                                          
+  initial_node_count = 3
   network            = google_compute_network.vpc_network_gke.self_link // Cluster deployed in ustom network 
   subnetwork         = google_compute_subnetwork.gke-subnet.self_link   // Cluster deployed in ustom subnetwork 
 
@@ -60,11 +71,11 @@ resource "google_container_cluster" "primary" { // creates google kubernetes clu
     update = "40m"
   }
 
-  #   master_auth {
-  #   client_certificate_config {
-  # issue_client_certificate = true
-  #    }
-  #  }
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = true
+     }
+   }
 
   cluster_autoscaling { // cluster is auto scaleable
     enabled = true
@@ -80,4 +91,57 @@ resource "google_container_cluster" "primary" { // creates google kubernetes clu
     }
   }
   depends_on = [google_compute_network.vpc_network_gke]
+}
+
+resource "kubernetes_namespace" "dev-namespace" {
+  metadata {
+    annotations = {
+      name = "dev-namespace"
+    }
+
+    labels = {
+      namespace = "dev"
+    }
+
+    name = "dev-namespace"
+  }
+}
+
+resource "kubernetes_namespace" "development" {
+  metadata {
+
+    labels = {
+      env = "dev"
+    }
+
+    name = "development"
+  }
+}
+
+resource "kubernetes_namespace" "staging" {
+  metadata {
+
+    labels = {
+      env = "stg"
+    }
+
+    name = "staging"
+  }
+}
+
+resource "kubernetes_namespace" "production" {
+  metadata {
+
+    labels = {
+      env = "prod"
+    }
+
+    name = "production"
+  }
+}
+
+locals {
+  cluster-certificate = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  cluster-endpoint = google_container_cluster.primary.endpoint
+  cluster-id = google_container_cluster.primary.id
 }
