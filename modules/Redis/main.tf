0b83f700741e18c090cbc5c790113084c9c48242
Uwaoma Ogude <uwaomaogude@NC-uwaomaogude-C02GC245Q6LT.local>
2022-03-02 00:08:46 +0200

    redis addition

--- modules/Redis/main.tf
+++ modules/Redis/main.tf
@@ -13,3 +13,40 @@ resource "google_compute_subnetwork" "redis-subnet" {
     ip_cidr_range = "192.168.5.0/24"
     }
 }
+
+
+# for the private service access that the redis instance needs for communication
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          = "private-ip-alloc"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network_redis.id
+}
+
+resource "google_service_networking_connection" "private_service_connection" {
+  network                 = google_compute_network.vpc_network_redis.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
+
+
+#redis instance 
+
+resource "google_redis_instance" "cache" {
+  name           = "private-cache"
+  tier           = "STANDARD_HA"
+  memory_size_gb = 1
+
+  location_id             = "europe-north1-a"
+  alternative_location_id = "us-central1-f"
+
+  authorized_network = google_compute_network.vpc_network_redis.id
+  connect_mode       = "PRIVATE_SERVICE_ACCESS"
+
+  redis_version     = "REDIS_4_0"
+  display_name      = "Terraform Test Instance"
+
+  depends_on = [google_service_networking_connection.private_service_connection]
+
+}
---
+++ modules/Redis/variables.tf
@@ -0,0 +1,4 @@
+variable "region" {
+  description = "network region"
+  type = string
+}
