cfd5e2ea15058e5d71ecc9f9becceabbdcf4b0ef
Talha <talha.khalid@aalto.fi>
2022-03-11 17:26:07 +0200

    space issue was the issue

--- profiles/GKE/variables.tf
+++ profiles/GKE/variables.tf
@@ -34,17 +34,3 @@ variable "zone" {
 
 
 
-/* if [ -d "env/$BRANCH_NAME/" ]; then
-    gcloud container clusters get-credentials gke-boutique-cluster --region europe-north1 --project team-2-a
-    namespaceStatus=$(kubectl get ns $BRANCH_NAME )
-    if [ $namespaceStatus == "Active" ]
-        then
-             echo "namespace is present"
-        else
-             kubectl create namespace $BRANCH_NAME
-    fi;
-else
-    echo "***************************** SKIPPING APPLYING *******************************"
-    echo "Branch '$BRANCH_NAME' does not represent an oficial environment."
-    echo "*******************************************************************************"
-fi; */
