3c4882fc3cf8221bf226b90bf73837beeaa764a5
Talha <talha.khalid@aalto.fi>
2022-03-11 16:22:50 +0200

    some progress

--- bash.sh
+++ bash.sh
@@ -3,7 +3,7 @@ gcloud components install kubectl
 if [ -d "env/$BRANCH_NAME/" ]; then
     gcloud container clusters get-credentials gke-boutique-cluster --region europe-north1 --project team-2-a
     namespaceStatus=$(kubectl get ns $BRANCH_NAME -o json )
-    if [ $namespaceStatus == "Active" ];
+    if [ $namespaceStatus == "Active" ]
         then
              echo "namespace is present"
         else
