0767da5fbd9f2232bb083f865d44fde5201e58f8
Wahiduzzaman Siddique <wahiduzzaman.siddique@nordcloud.com>
2022-03-11 13:15:59 +0200

    cloudbuild timeout and kubernestes provider code updated

--- profiles/gke/provider.tf
+++ profiles/gke/provider.tf
@@ -13,6 +13,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
+  load_config_file = "false"
   host  = "https://${module.gke_cluster.output-cluster-endpoint}"
   token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
