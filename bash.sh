fcef07021f0ddc6a78b2d1aea59f5a8b144c172f
Talha <talha.khalid@aalto.fi>
2022-03-14 13:06:15 +0200

    awsdf

--- bash.sh
+++ bash.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 gcloud components install kubectl
+sudo apt-get update
+sudo apt-get install jq
+
 if [ -d "env/$BRANCH_NAME/" ]; then
     gcloud container clusters get-credentials gke-boutique-cluster --region europe-north1 --project team-2-a
     namespaceStatus=$(kubectl get ns $BRANCH_NAME -o json | jq .status.phase -r)
