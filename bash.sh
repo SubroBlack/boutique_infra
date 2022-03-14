8a7ef4a21a958c183943cbbf314f7dafaea29fad
Talha <talha.khalid@aalto.fi>
2022-03-14 13:18:00 +0200

    awsdf

--- bash.sh
+++ bash.sh
@@ -6,7 +6,7 @@ apt-get install -y jq
 if [ -d "env/$BRANCH_NAME/" ]; then
     gcloud container clusters get-credentials gke-boutique-cluster --region europe-north1 --project team-2-a
     namespaceStatus=$(kubectl get ns $BRANCH_NAME -o json | jq .status.phase -r)
-    if ["$namespaceStatus" = "Active"]
+    if ["$namespaceStatus"="Active"]
         then
              echo "namespace is present"
         else
